124d01355476882f914474e8dba1fd73199e5508
middtho-dev <middtho@yandex.ru>
2025-03-30 13:01:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,15 +10,15 @@ GITHUB_REPO="middtho-dev/frp-router"
 FRP_DIR="/root/frp"
 INIT_SCRIPT="/etc/init.d/frpc"
 
-# Скачиваем файлы frpc и frpc.toml
+# Скачиваем файл frpc
 mkdir -p $FRP_DIR
 cd $FRP_DIR
 
-echo -e "${GREEN}Скачиваю frpc и frpc.toml с GitHub...${NC}"
-if curl -L "https://github.com/$GITHUB_REPO/raw/main/frpc" -o "frpc" && curl -L "https://github.com/$GITHUB_REPO/raw/main/frpc.toml" -o "frpc.toml"; then
-    echo -e "${GREEN}Файлы успешно скачаны.${NC}"
+echo -e "${GREEN}Скачиваю frpc с GitHub...${NC}"
+if curl -L "https://github.com/$GITHUB_REPO/raw/main/frpc" -o "frpc"; then
+    echo -e "${GREEN}Файл frpc успешно скачан.${NC}"
 else
-    echo -e "${RED}Ошибка при загрузке файлов с GitHub. Проверьте URL.${NC}"
+    echo -e "${RED}Ошибка при загрузке файла frpc с GitHub.${NC}"
     exit 1
 fi
 
@@ -28,6 +28,45 @@ if chmod +x $FRP_DIR/frpc; then
     echo -e "${GREEN}Права успешно установлены на frpc.${NC}"
 else
     echo -e "${RED}Не удалось установить права на frpc.${NC}"
+    exit 1
+fi
+
+# Запрашиваем параметры для frpc.toml
+echo -e "${GREEN}Введите имя для прокси Luci (например: Home Luci):${NC}"
+read luci_name
+echo -e "${GREEN}Введите порт для прокси Luci (например: 8081):${NC}"
+read luci_port
+
+echo -e "${GREEN}Введите имя для прокси SSH (например: Home SSH):${NC}"
+read ssh_name
+echo -e "${GREEN}Введите порт для прокси SSH (например: 2201):${NC}"
+read ssh_port
+
+# Создаем frpc.toml
+echo -e "${GREEN}Создаю файл frpc.toml...${NC}"
+cat <<EOF > $FRP_DIR/frpc.toml
+serverAddr = "router.kv9.ru"
+serverPort = 7000
+
+[[proxies]]
+name = "$luci_name"
+type = "tcp"
+localPort = 80
+remotePort = $luci_port
+
+[[proxies]]
+name = "$ssh_name"
+type = "tcp"
+localIP = "127.0.0.1"
+localPort = 22
+remotePort = $ssh_port
+EOF
+
+# Проверка на успешное создание frpc.toml
+if [[ -f $FRP_DIR/frpc.toml ]]; then
+    echo -e "${GREEN}Файл frpc.toml успешно создан.${NC}"
+else
+    echo -e "${RED}Не удалось создать файл frpc.toml.${NC}"
     exit 1
 fi
 
